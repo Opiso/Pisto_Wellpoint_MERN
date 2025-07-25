c693d9dcb000842b9e38a7449f423d3dae7a852d
Capis Capis <capisokoth@gmail.com>
2025-07-25 16:53:34 +0300

    for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const dbConfig = require('./config/dbConfig');
-const userRoute = require('./routes/userRoute');
-const adminRoute = require('./routes/adminRoute');
-const doctorRoute = require('./routes/doctorRoute');
-const appointmentRoute = require('./routes/appointmentRoute');
+const express = require("express");
+const cors = require("cors");
+const dbConfig = require("./config/dbConfig");
+const userRoute = require("./routes/userRoute");
+const adminRoute = require("./routes/adminRoute");
+const doctorRoute = require("./routes/doctorRoute");
+const appointmentRoute = require("./routes/appointmentRoute");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
- 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://pisto-wellpoint.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-app.use('/api/user', userRoute);
-app.use('/api/admin', adminRoute);
-app.use('/api/doctor', doctorRoute);  
-app.use('/api/appointment', appointmentRoute);
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorRoute);
+app.use("/api/appointment", appointmentRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on Port ${PORT}`);
 });
-
