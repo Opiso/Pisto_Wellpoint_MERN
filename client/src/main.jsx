9be6701864a0f994ab98943b0219fc3f33b53e36
Capis Capis <capisokoth@gmail.com>
2025-08-01 20:31:45 +0300

    Include frontend build for deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {Provider} from 'react-redux';
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import App from "./App.jsx";
 // import reportwebvitals from './reportwebvitals'
-import store from './redux/store';
+import store from "./redux/store";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
