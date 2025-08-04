898a89cc35b29c4a91c7b5f4b900575986d7ba40
Capis Capis <capisokoth@gmail.com>
2025-08-04 11:08:32 +0300

    Fixing the react router refresh problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,7 @@ import RedirectionPage from "./components/redirectionPage";
 function App() {
   const { loading } = useSelector((state) => state.alerts);
   return (
-    <BrowserRouter  basename = "/Pisto_Wellpoint_MERN">
+    <BrowserRouter >
       {loading && (
         <div className="loader-parent m-3 bg-gray-100">
           <div className="loader"></div>
